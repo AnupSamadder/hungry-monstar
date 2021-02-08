c7d9ba964ee506dd7cd0088d1921b4d6d0dbc7e3
Anup Samadder <anup.samadder07@gmail.com>
2021-02-08 12:05:13 +0600

    m

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-.then( res => res.json())
-.then( data => console.log(data.meals[0]))
+const searchMeal = () =>{
+    const searchInput = document.getElementById("search-input").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+    fetch(url)
+    .then( res => res.json())
+    .then( data => console.log(data))
+}
